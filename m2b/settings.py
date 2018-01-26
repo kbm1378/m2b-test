a0c98696df5f557a490c2b81d34a81c734bd9058
byungmook <kbm1378@gmail.com>
2018-01-27 03:01:59 +0900

    rds db setting

--- m2b/settings.py
+++ m2b/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'm2b.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'm2b',
+	'USER': 'myals93',
+	'PASSWORD': 'bluespring!2',
+	'HOST': 'm2b.cipvjqqbblly.us-east-2.rds.amazonaws.com',
+    	'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
     }
 }
-
 
 
 # Password validation
