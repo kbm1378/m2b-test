fc68242f7e1d2ff8e142a09739d1822a3311d734
byungmook <kbm1378@gmail.com>
2018-01-27 03:49:05 +0900

    remove compressor

--- m2b/settings.py
+++ m2b/settings.py
@@ -140,5 +140,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
 )
