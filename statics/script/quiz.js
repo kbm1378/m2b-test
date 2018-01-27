aabffcbe976b72372bf3df9468eb8bc4c00281f7
byungmook <kbm1378@gmail.com>
2018-01-28 04:10:36 +0900

    실제 이미지 적용

--- statics/script/quiz.js
+++ statics/script/quiz.js
@@ -89,12 +89,13 @@ baseApp.controller("BaseCtrl", [
             '1. 좋아하는 색은?',
             '향의 또 다른 언어는 이미지입니다. 색으로 나를 표현하는 것은 향으로 나를 표현하는 첫 단계입니다.',
             [
-                new QuizItem(1, '빨강', 'https://i.imgur.com/gbMEtRN.jpg'),
-                new QuizItem(2, '노랑', 'https://i.imgur.com/N8l6Rbw.jpg'),
-                new QuizItem(3, '초록', 'https://i.imgur.com/uWMBWys.jpg'),
-                new QuizItem(4, '파랑', 'https://i.imgur.com/k7JBfoV.jpg'),
-                new QuizItem(5, '검정', 'https://i.imgur.com/G9CnmVB.jpg'),
-                new QuizItem(6, '흰색', '')
+                new QuizItem(1, '빨강', 'https://i.imgur.com/4EnPXGX.png'),
+                new QuizItem(2, '노랑', 'https://i.imgur.com/1BBWSse.png'),
+                new QuizItem(3, '초록', 'https://i.imgur.com/EBpDudb.png'),
+                new QuizItem(4, '파랑', 'https://i.imgur.com/4wqe2v5.png'),
+                new QuizItem(5, '갈색', 'https://i.imgur.com/4t36a1i.png'),
+                new QuizItem(6, '검정', 'https://i.imgur.com/ocBENIk.png')
+
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -103,12 +104,12 @@ baseApp.controller("BaseCtrl", [
             '2. 다니고 있는 직장은?',
             '향수는 개인적인 아이템이 아니라 당신 주변의 사람과 공유하는 아이템입니다.',
             [
-                new QuizItem(1, '영업직', 'https://i.imgur.com/gbMEtRN.jpg'),
-                new QuizItem(2, '사무직', 'https://i.imgur.com/N8l6Rbw.jpg'),
-                new QuizItem(3, '연구직', 'https://i.imgur.com/uWMBWys.jpg'),
-                new QuizItem(4, '전문직', 'https://i.imgur.com/k7JBfoV.jpg'),
-                new QuizItem(5, '사업가', 'https://i.imgur.com/G9CnmVB.jpg'),
-                new QuizItem(6, '기타', '')
+                new QuizItem(1, '영업직', 'https://i.imgur.com/UY3qnHj.png'),
+                new QuizItem(2, '사무직', 'https://i.imgur.com/nHm1Mfx.png'),
+                new QuizItem(3, '연구직', 'https://i.imgur.com/i0UX53g.png'),
+                new QuizItem(4, '전문직', 'https://i.imgur.com/HiS8OT4.png'),
+                new QuizItem(5, '사업가', 'https://i.imgur.com/jycI7Yc.png'),
+                new QuizItem(6, '기타', 'https://i.imgur.com/pQZnwar.png')
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -117,8 +118,8 @@ baseApp.controller("BaseCtrl", [
             '3. 가지고 있는 취미는?',
             '취미의 환경에 적합한 향수를 추천해드립니다.',
             [
-                new QuizItem(1, '사람들과 함께 하는 취미', 'https://i.imgur.com/nx7ZUxC.jpg'),
-                new QuizItem(2, '혼자서 하는 취미', 'https://i.imgur.com/MHjEJWh.jpg')
+                new QuizItem(1, '사람들과 함께 하는 취미', 'https://i.imgur.com/ZKrETty.png'),
+                new QuizItem(2, '혼자서 하는 취미', 'https://i.imgur.com/3fFkAJk.png')
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -127,9 +128,9 @@ baseApp.controller("BaseCtrl", [
             '4. 좋아하는 음식의 분류는?',
             '고지방 음식을 좋아하는 사람은 피부의 유분으로 인해 강하고 오래가는 향이 어울립니다.',
             [
-                new QuizItem(1, '채식', 'https://i.imgur.com/Q13kQaY.jpg'),
-                new QuizItem(2, '균형', 'https://i.imgur.com/Kt6PSGW.jpg'),
-                new QuizItem(3, '육식', 'https://i.imgur.com/XJSwvbl.jpg'),
+                new QuizItem(1, '채식', 'https://i.imgur.com/jy1VRxy.png'),
+                new QuizItem(2, '균형', 'https://i.imgur.com/I0kiwyy.png'),
+                new QuizItem(3, '육식', 'https://i.imgur.com/B1LZHS7.png'),
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -138,11 +139,11 @@ baseApp.controller("BaseCtrl", [
             '5. 자주 입는 패션 스타일은?',
             '패션 스타일은 향수의 선호도를 파악하는 좋은 지표입니다.',
             [
-                new QuizItem(1, '하이패션', 'https://i.imgur.com/HCDqDah.jpg'),
-                new QuizItem(2, '캐주얼', 'https://i.imgur.com/nhW9pp2.jpg'),
-                new QuizItem(3, '프레피', 'https://i.imgur.com/IPnXqdG.jpg'),
-                new QuizItem(4, '슈트', 'https://i.imgur.com/3eEXmYR.jpg'),
-                new QuizItem(5, '스트릿', 'https://i.imgur.com/JxX2UNr.jpg'),
+                new QuizItem(1, '하이패션', 'https://i.imgur.com/sFbfyaR.png'),
+                new QuizItem(2, '캐주얼', 'https://i.imgur.com/v8DYvB9.png'),
+                new QuizItem(3, '프레피', 'https://i.imgur.com/ys2pFPq.png'),
+                new QuizItem(4, '슈트', 'https://i.imgur.com/YSofa1L.png'),
+                new QuizItem(5, '스트릿', 'https://i.imgur.com/8OIahhb.png'),
                 new QuizItem(0, '', 'https://i.imgur.com/pQZnwar.png')
             ]
         ));
@@ -152,11 +153,11 @@ baseApp.controller("BaseCtrl", [
             '6. 주로 나타나는 성격은?',
             '연구에 의하면 성격은 환경에 대한 자극을 반영합니다. 내향적인 사람일수록 따뜻하고, 복잡한 향을 선호합니다.',
             [
-                new QuizItem(1, '매우 외향적', 'https://i.imgur.com/HCDqDah.jpg'),
-                new QuizItem(2, '외향적', 'https://i.imgur.com/nhW9pp2.jpg'),
-                new QuizItem(3, '보통', 'https://i.imgur.com/IPnXqdG.jpg'),
-                new QuizItem(4, '내향적', 'https://i.imgur.com/3eEXmYR.jpg'),
-                new QuizItem(5, '매우 내향적', 'https://i.imgur.com/JxX2UNr.jpg'),
+                new QuizItem(1, '매우 외향적', 'https://i.imgur.com/9sjb29y.png'),
+                new QuizItem(2, '외향적', 'https://i.imgur.com/PM4pVCQ.png'),
+                new QuizItem(3, '보통', 'https://i.imgur.com/VHZFVm8.png'),
+                new QuizItem(4, '내향적', 'https://i.imgur.com/RXkZse2.png'),
+                new QuizItem(5, '매우 내향적', 'https://i.imgur.com/kl1tSOH.png'),
                 new QuizItem(0, '', 'https://i.imgur.com/pQZnwar.png')
             ]
         ));
@@ -166,9 +167,9 @@ baseApp.controller("BaseCtrl", [
             '7. 몸에 열이 나는 정도는?',
             '땀에 포함된 화학성분은 향에 큰 영향을 미칩니다. 높은 체온을 가진 사람일수록 향수가 더욱 빨리 날아갑니다.',
             [
-                new QuizItem(1, '차가운 편', 'https://i.imgur.com/TBhy68F.png'),
-                new QuizItem(2, '보통', 'https://i.imgur.com/B9Mu37U.png'),
-                new QuizItem(3, '뜨거운 편', 'https://i.imgur.com/ulXhQ9Y.png')
+                new QuizItem(1, '차가운 편', 'https://i.imgur.com/nNRucDr.png'),
+                new QuizItem(2, '보통', 'https://i.imgur.com/kaCC15M.png'),
+                new QuizItem(3, '뜨거운 편', 'https://i.imgur.com/7WYi3gu.png')
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -177,9 +178,9 @@ baseApp.controller("BaseCtrl", [
             '8. 여유가 있을때 주로 무엇을?',
             '당신의 라이프를 즐기는 곳은 얼마나 붐빕니까? 향은 당신 앞에 있는 사람과 함께하는 것입니다.',
             [
-                new QuizItem(1, '클럽/파티', 'https://i.imgur.com/Fom4Hox.png'),
-                new QuizItem(2, '혼자서 보내는 여유', 'https://i.imgur.com/5jZG8wl.png'),
-                new QuizItem(3, '친구/연인과의 데이트', 'https://i.imgur.com/ulXhQ9Y.png')
+                new QuizItem(1, '클럽/파티', 'https://i.imgur.com/xeympFV.png'),
+                new QuizItem(2, '혼자서 보내는 여유', 'https://i.imgur.com/g9AZywH.png'),
+                new QuizItem(3, '친구/연인과의 데이트', 'https://i.imgur.com/2tGsiVE.png')
             ]
         ));
         $scope.quizQuestionList.push(new QuizQuestion(
@@ -188,8 +189,8 @@ baseApp.controller("BaseCtrl", [
             '9. 성별',
             '성별에 따라 당신을 표현하는 방법이 달라집니다.',
             [
-                new QuizItem(1, '남성', 'https://i.imgur.com/XuHH2zL.jpg'),
-                new QuizItem(2, '여성', 'https://i.imgur.com/9Bz0oRn.jpg')
+                new QuizItem(1, '남성', 'https://i.imgur.com/liZ1Xuv.png'),
+                new QuizItem(2, '여성', 'https://i.imgur.com/nLN9mwP.png')
             ],
             true
         ));
